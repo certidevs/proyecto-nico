48e706f2a757d57258368617d08a437c7283e836
nhurtadoc1 <nhurtadoc1@alumnos.nebrija.es>
2026-06-15 19:20:23 +0200

    Baseline

--- src/main/java/com/demo/model/Song.java
+++ src/main/java/com/demo/model/Song.java
@@ -1,5 +1,38 @@
 package com.demo.model;
-//test
-public class Song {
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter // lombok
+@Setter // lombok
+@ToString // lombok
+@NoArgsConstructor // lombok: crea el constructor vacío sin argumentos
+@AllArgsConstructor // lombok: crea el constructor con todos los params
+@Builder
+@Table(name = "canciones")
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String title;
+
+    private Integer duration;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) // yyyy-MM-dd
+    private LocalDate releaseDate = LocalDate.now();
+
+    private String artist;
+
+    private Integer popularity;
+
+    public Song(String title, String artist, Integer duration) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
 }
